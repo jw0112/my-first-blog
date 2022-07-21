8818e49775ae3765de5c62b0b266494b26a56312
jw0112 <a01086387102@gmail.com>
2022-07-21 23:04:48 +0900

    Added view and template for detailed blog post as well as CSS for the site

--- FirstProject/firstproject/blog/views.py
+++ FirstProject/firstproject/blog/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.utils import timezone
 # 현재 directory에 있는 models.py 파일에 정의된 모델을 가져오기
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
-    
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})    
