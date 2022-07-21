6abfe09fc8f1211b2bdf38d3767a07b716cb3f88
jw0112 <a01086387102@gmail.com>
2022-07-21 21:47:52 +0900

    Modified templates to display posts from database.

--- FirstProject/firstproject/blog/views.py
+++ FirstProject/firstproject/blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+# 현재 directory에 있는 models.py 파일에 정의된 모델을 가져오기
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
+    
